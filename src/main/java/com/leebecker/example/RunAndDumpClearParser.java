069de9bed49b60373e2e696c93fd8dcd8002f172
lee.becker@gmail.com <lee.becker@gmail.com>
2013-07-04 22:37:38 -0600

    Added options, to make Run* files look more like actual wrappers.

--- src/main/java/com/leebecker/example/RunAndDumpClearParser.java
+++ src/main/java/com/leebecker/example/RunAndDumpClearParser.java
@@ -5,26 +5,35 @@ import java.io.IOException;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.collection.CollectionReader;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.cleartk.clearnlp.DependencyParser;
 import org.cleartk.clearnlp.MPAnalyzer;
 import org.cleartk.clearnlp.PosTagger;
 import org.cleartk.clearnlp.Tokenizer;
-import org.cleartk.syntax.dependency.malt.MaltParser;
 import org.cleartk.syntax.opennlp.SentenceAnnotator;
-import org.cleartk.token.tokenizer.TokenAnnotator;
+import org.cleartk.util.Options_ImplBase;
 import org.cleartk.util.ae.UriToDocumentTextAnnotator;
 import org.cleartk.util.cr.UriCollectionReader;
+import org.kohsuke.args4j.Option;
 import org.uimafit.factory.AggregateBuilder;
 import org.uimafit.factory.CollectionReaderFactory;
 import org.uimafit.pipeline.SimplePipeline;
 
 public class RunAndDumpClearParser {
 	
+	public static class Options extends Options_ImplBase {
+		
+		@Option(name="--input-dir")
+		public File inputDir = new File("/tmp/files_to_parse");
+		
+	}
+	
 	public static void main (String[] args) throws UIMAException, IOException {
 		
 		
-		File inputDir = new File("/tmp/files_to_parse");
+		Options options = new Options();
+		options.parseOptions(args);
+		
+		File inputDir = options.inputDir;
 		
 		CollectionReader reader = CollectionReaderFactory.createCollectionReader(
 				UriCollectionReader.class, 
--- src/main/java/com/leebecker/example/RunAndDumpMaltParser.java
+++ src/main/java/com/leebecker/example/RunAndDumpMaltParser.java
@@ -5,23 +5,33 @@ import java.io.IOException;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.collection.CollectionReader;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.cleartk.opennlp.POSTagger;
 import org.cleartk.syntax.dependency.malt.MaltParser;
 import org.cleartk.syntax.opennlp.SentenceAnnotator;
 import org.cleartk.token.tokenizer.TokenAnnotator;
+import org.cleartk.util.Options_ImplBase;
 import org.cleartk.util.ae.UriToDocumentTextAnnotator;
 import org.cleartk.util.cr.UriCollectionReader;
+import org.kohsuke.args4j.Option;
 import org.uimafit.factory.AggregateBuilder;
 import org.uimafit.factory.CollectionReaderFactory;
 import org.uimafit.pipeline.SimplePipeline;
 
 public class RunAndDumpMaltParser {
 	
+	public static class Options extends Options_ImplBase {
+		
+		@Option(name="--input-dir")
+		public File inputDir = new File("/tmp/files_to_parse");
+		
+	}
+	
 	public static void main (String[] args) throws UIMAException, IOException {
 		
+		Options options = new Options();
+		options.parseOptions(args);
 		
-		File inputDir = new File("/tmp/files_to_parse");
+		File inputDir = options.inputDir;
 		
 		CollectionReader reader = CollectionReaderFactory.createCollectionReader(
 				UriCollectionReader.class, 
@@ -33,7 +43,7 @@ public class RunAndDumpMaltParser {
 		pipeline.add(SentenceAnnotator.getDescription());
 		pipeline.add(TokenAnnotator.getDescription());
 		pipeline.add(POSTagger.getDescription("en"));
-		pipeline.add(MaltParser.getDescription("/tmp/models/engmalt.linear-1.7.mco"));
+		pipeline.add(MaltParser.getDescription());
 		pipeline.add(DumpDependencyParsesAnnotator.getDescription());
 		
 		SimplePipeline.runPipeline(reader, pipeline.createAggregate());
